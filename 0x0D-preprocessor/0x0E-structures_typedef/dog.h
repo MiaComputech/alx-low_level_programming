590b3e193140c518b398bae8445c9137238a8088
MiaComputech <MiaComputechplc@gmail.com>
2022-07-05 05:05:59 +0300

    Task 0

--- 0x0D-preprocessor/0x0E-structures_typedef/dog.h
+++ 0x0D-preprocessor/0x0E-structures_typedef/dog.h
@@ -1,28 +1,23 @@
-#ifndef FILE_DOG
-#define FILE_DOG
-
+#ifndef _DOG_H_
+#define _DOG_H_
 /**
- * struct dog - structure dog
- * @name: atribute pointer to char name's dog
- * @age: atribute float age's dog
- * @owner: atribute pointer to char owner's dog
- */
+ * struct dog -Dog data.
+ * @name: Name of the dog
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ * Desctiption: a structure with the name, age and owner of a dog.
+**/
 
-struct dog
+typedef struct dog
 {
-	char *name;
-	float age;
-	char *owner;
-};
-
-typedef struct dog dog_t;
-
+char *name;
+char *owner;
+float age;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
-
 dog_t *new_dog(char *name, float age, char *owner);
-
 void free_dog(dog_t *d);
-
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
 #endif
