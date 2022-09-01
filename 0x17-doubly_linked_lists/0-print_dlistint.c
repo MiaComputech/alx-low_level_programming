38f258fdc7bd02d77ab75463a938c9a270b4891f
MiaComputech <MiaComputechplc@gmail.com>
2022-09-01 21:54:32 +0300

    Task init

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,13 +1,11 @@
 #include "lists.h"
 #include <stdio.h>
-
 /**
  * print_dlistint - print elements of doubly linked list
  * @h: linked list
  *
  * Return: number of nodes
  */
-
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nbr_nodes = 0;
